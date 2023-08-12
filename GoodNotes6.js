7693204901b7b8f1adbed43d193bb0afe1067457
kikoxvx <65149479+kikoxvx@users.noreply.github.com>
2023-08-12 12:26:28 +0800

    Delete GoodNotes6.js

--- GoodNotes6.js
+++
@@ -1,8 +0,0 @@
-[rewrite_local] 
-^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-echo-response https://raw.githubusercontent.com/kikoxvx/QuantumultX/rewrite/GoodNotes6.js
-^https:\/\/isi\.csan\.goodnotes\.com\/.+\/subscribers\/[^/]+/(offerings|attributes)$ url request-header (\r\n)X-RevenueCat-ETag:.+(\r\n) request-header $1X-RevenueCat-ETag:$2
-
-[mitm] 
-hostname=isi.csan.goodnotes.com
-
-
