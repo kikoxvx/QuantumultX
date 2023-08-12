3c7d4d3b9c24c092b1736a8856516023b53f8709
kikoxvx <65149479+kikoxvx@users.noreply.github.com>
2023-08-12 12:24:09 +0800

    Update GoodNotes6.js

--- GoodNotes6.js
+++ GoodNotes6.js
@@ -1,5 +1,5 @@
 [rewrite_local] 
-^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-echo-response https://github.com/kikoxvx/QuantumultX/raw/rewrite/GoodNotes6.js
+^https:\/\/isi\.csan\.goodnotes\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-echo-response https://raw.githubusercontent.com/kikoxvx/QuantumultX/rewrite/GoodNotes6.js
 ^https:\/\/isi\.csan\.goodnotes\.com\/.+\/subscribers\/[^/]+/(offerings|attributes)$ url request-header (\r\n)X-RevenueCat-ETag:.+(\r\n) request-header $1X-RevenueCat-ETag:$2
 
 [mitm] 
